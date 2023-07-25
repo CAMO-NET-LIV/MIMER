890ac34d8d277d4b8ddbb0ce6febc422d8057c58
anoopvelluv <121963285+anoopvelluv@users.noreply.github.com>
2023-07-25 14:46:02 +0100

    Initial Commit
    Added Core Functionality

---
+++ R/common_utils.R
@@ -0,0 +1,53 @@
+
+clean_medication <- function(df,column_name) {
+  df %>%
+    distinct({{column_name}}, .keep_all = FALSE) %>%
+    filter({{column_name}} != "") %>%
+    mutate(medication_cleaned = sapply(strsplit({{column_name}}, " "),`[`,1)) %>%
+
+    filter(! stringr::str_detect({{column_name}},"(?!)Desensitization")) %>%
+    mutate(medication_cleaned = trimws(.data$medication_cleaned))
+
+}
+
+populate_amr_antibiotics <- function(){
+  #Loading Anti-Biotics Data from AMR Package
+  df_amr_antibiotics <- as.data.frame(AMR::antibiotics)
+
+  df_amr_antibiotics_lookup <- df_amr_antibiotics %>%
+    select(.data$name, .data$synonyms) %>%
+    tidyr::unnest(.data$synonyms)
+
+  df_amr_antibiotics_lookup_copy <-  data.frame(name  = distinct(df_amr_antibiotics,.data$name, .keep_all = FALSE),
+                                                synonyms = distinct(df_amr_antibiotics,.data$name, .keep_all = FALSE) )%>%
+    rename("synonyms" =.data$name.1) %>%
+    mutate(synonyms = stringr::str_replace(.data$synonyms," acid",""))
+
+  df_amr_antibiotics_lookup_final <- rbind(df_amr_antibiotics_lookup_copy,df_amr_antibiotics_lookup) %>% filter(! .data$synonyms=='aminox')
+
+  df_amr_antibiotics_lookup_final
+
+}
+
+
+add_new_synonyms <- function(amr_df, new_synonyms){
+  new_df <- data.frame(name  = new_synonyms,
+                       synonyms = new_synonyms)
+
+  rbind(amr_df,new_df)
+}
+
+get_antibiotics_in_medications <- function (df_medications_cleaned,df_amr_antibiotics_lookup,fuzzy_matching_method){
+ df_antibiotics_in_df <- fuzzyjoin::stringdist_inner_join(
+    df_medications_cleaned,
+    df_amr_antibiotics_lookup,
+    by = c("medication_cleaned" = "synonyms"),
+    ignore_case = TRUE,
+    method=fuzzy_matching_method,
+    distance_col = "distance_column"
+  ) %>%
+    filter(.data$synonyms != "")
+
+  df_antibiotics_in_df
+}
+
